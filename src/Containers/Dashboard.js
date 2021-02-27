22d6ed51fa87c763fd90358fdcdd2ce1c34464fc
irosen419 <irosen419@gmail.com>
2021-02-27 14:00:32 -0500

    fetch alumn data from dashboard

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
 function Dashboard() {
-    return (
-        <div>DASBOARD</div>
-    )
+
+  const [alumns, setAlumns] = useState([])
+
+  useEffect(() => {
+    fetchAlumns()
+  }, [])
+
+  const fetchAlumns = () => {
+    fetch('http://localhost:3000/api/v1/alumns')
+      .then(res => res.json())
+      .then((alumnsArray) => setAlumns(alumnsArray))
+  }
+
+  return (
+    <div>
+      {console.log(alumns)}
+    </div>
+  )
 }
 
 export default Dashboard
