fb9f938bf515e100099dbe20c5e83b5e3e6c0156
irosen419 <irosen419@gmail.com>
2021-05-13 13:36:19 -0400

    deconstruct code on search result

--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -1,5 +1,4 @@
-export function SearchResult({ node, focusFxn }) {
-  const { id, alumn_id, x, y } = node
+export function SearchResult({ node: { id, alumn_id, x, y }, focusFxn }) {
   return <li
     className="search-result"
     onClick={() => focusFxn(alumn_id, x, y)}>
