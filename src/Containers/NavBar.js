b74a6f95f4c82e51269e93cfb9ead80ae7afcdc9
Ivan Cuadra <ivan.cuadra.93@gmail.com>
2023-05-19 15:04:17 -0700

    Add get admins query

--- src/Containers/NavBar.js
+++ src/Containers/NavBar.js
@@ -1,11 +1,69 @@
-import { useContext } from 'react'
-import { Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { AdminContext } from '../Context/Context'
+import { useContext, useEffect, useState } from 'react';
+import { Nav, Toast } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AdminContext } from '../Context/Context';
 
 function NavBar({ logout }) {
+    const baseUrl = process.env.REACT_APP_BASE_URL;
 
-    const admin = useContext(AdminContext)
+    const admin = useContext(AdminContext);
+
+    const [isSysAdmin, setIsSysAdmin] = useState(false);
+    const [showGetAdminsQueryErrorToast, setShowGetAdminsQueryErrorToast] = useState(false);
+    const [showGetAdminsQuerySuccessfulToast, setShowGetAdminsQuerySuccessfulToast] = useState(false);
+    useEffect(() => {
+        const fetchAdminStatus = async () => {
+            try {
+                const token = localStorage.getItem('jwt');
+
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                };
+
+                const response = await fetch(`${baseUrl}/admins/check_admin_status`, options);
+                if (response.ok) {
+                    const data = await response.json();
+                    setIsSysAdmin(data);
+                } else {
+                    console.error('Error:', response.status);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchAdminStatus();
+    }, [baseUrl]);
+
+    const handleGetAdminsClick = () => {
+        const fetchAdminQuery = async () => {
+            try {
+                const token = localStorage.getItem('jwt');
+
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                };
+
+                const response = await fetch(`${baseUrl}/admins/get_admins`, options);
+                setShowGetAdminsQueryErrorToast(false);
+                if (response.ok) {
+                    setShowGetAdminsQuerySuccessfulToast(true);
+                } else {
+                    setShowGetAdminsQueryErrorToast(true);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchAdminQuery();
+    };
 
     return (
         <div
@@ -23,13 +81,56 @@ function NavBar({ logout }) {
                     <Nav.Item className="nav-item ml-4">
                         <Link to="/dashboard">Dashboard</Link>
                     </Nav.Item>
+                    {isSysAdmin ? (
+                        <Nav.Item className="nav-item ml-4">
+                            <Link onClick={handleGetAdminsClick} to={`/graph/${admin.labId}`}>
+                                Get Admins
+                            </Link>
+                        </Nav.Item>
+                    ) : (
+                        <></>
+                    )}
                 </div>
                 <Nav.Item className="nav-item" onClick={() => logout()}>
                     <Link to="#">Logout</Link>
                 </Nav.Item>
             </Nav>
+            <Toast
+                style={{
+                    position: 'absolute',
+                    top: 10,
+                    backgroundColor: "red"
+                }}
+                animation={true}
+                show={showGetAdminsQueryErrorToast}
+                onClose={() => setShowGetAdminsQueryErrorToast(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">Error</strong>
+                    <small>now</small>
+                </Toast.Header>
+                <Toast.Body>
+                    There has been an error.
+                </Toast.Body>
+            </Toast>
+            <Toast
+                style={{
+                    position: 'absolute',
+                    top: 10,
+                    backgroundColor: "green"
+                }}
+                animation={true}
+                show={showGetAdminsQuerySuccessfulToast}
+                onClose={() => setShowGetAdminsQuerySuccessfulToast(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">Success!</strong>
+                    <small>now</small>
+                </Toast.Header>
+                <Toast.Body>
+                    The query has been successfully send to your email!
+                </Toast.Body>
+            </Toast>
         </div>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;;
