e0848cb301ec9afca206543e1fd357258bf26c46
Ivan Cuadra <ivan.cuadra.93@gmail.com>
2023-09-05 15:58:59 -0700

    Style Menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,10 @@
 import { useContext } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+
 import { AdminContext } from '../Context/Context';
+
+import '../styles/Menu.css';
 
 function Menu({ show }) {
     let navigate = useNavigate();
@@ -11,16 +14,9 @@ function Menu({ show }) {
 
     return (
         <Dropdown style={{
-            position: 'absolute',
-            right: 30,
-            top: 30,
-            height: 'fit-content',
-            zIndex: 500,
-            borderRadius: '.25rem',
-            boxShadow: '-1px 1px 10px rgb(31, 31, 31)',
             display: show ? "block" : "none"
         }}>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            <Dropdown.Toggle className='button' id="dropdown-basic">
                 Menu
             </Dropdown.Toggle>
 
